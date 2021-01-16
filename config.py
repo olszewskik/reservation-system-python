d47c0bf59dcc09ff0081d2e06fc23112303ae786
Kamil Olszewski <kamil.web@gmail.com>
2021-01-16 14:20:46 +0100

    mysql database add

--- config.py
+++ config.py
@@ -13,8 +13,9 @@ class Config():
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get(
+    #     'DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+    SQLALCHEMY_DATABASE_URI = 'mysql://root@localhost/rsdev'
 
 
 class TestingConfig(Config):
--- reservation_system.py
+++ reservation_system.py
@@ -1,6 +1,8 @@
 import os
-from app import create_app, db
 from flask_migrate import Migrate
+from app import create_app, db
+from app.models import User, Role
+
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
@@ -8,4 +10,4 @@ migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict()
+    return dict(db=db, User=User, Role=Role)
