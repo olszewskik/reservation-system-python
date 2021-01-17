a3b99fa69676d095cee6ba8869d4d037493289eb
Kamil Olszewski <kamil.web@gmail.com>
2021-01-17 18:15:17 +0100

    add user auth

--- app/__init__.py
+++ app/__init__.py
@@ -19,6 +19,9 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
     # with app.app_context():
     #     db.create_all()
 
