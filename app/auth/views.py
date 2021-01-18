756d283c53f21cc8f557cfe1c5eebdb3cfb2aade
Kamil Olszewski <kamil.web@gmail.com>
2021-01-18 22:33:50 +0100

    add user auth

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,10 @@
 from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, logout_user, login_required
+
 from . import auth
-from ..models import User
 from .forms import LoginForm, RegistrationForm
 from .. import db
+from ..models import User
 
 
 @auth.route('/login', methods=['GET', 'POST'])
--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
-from . import db, login_manager
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from . import db, login_manager
 
 
 class Role(db.Model):
